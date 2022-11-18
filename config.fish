c35ce60c7964bdb0564a72f9554cc070c5b572bc
Igor Gunin <ip.gunin@pelerin-solutions.ru>
2022-11-18 16:16:11 +0300

    reinitial

---
+++ config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+rvm default &> /dev/null
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showupstream informative
+set -g __fish_git_prompt_showdirtystate yes
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_cleanstate ' ✔ '
+set -g __fish_git_prompt_char_dirtystate ' ✚ '
+set -g __fish_git_prompt_char_invalidstate ' ✖ '
+set -g __fish_git_prompt_char_stagedstate ' ● '
+set -g __fish_git_prompt_char_stashstate ' ⚑ '
+set -g __fish_git_prompt_char_untrackedfiles ' ? '
+set -g __fish_git_prompt_char_upstream_ahead ' ↑ '
+set -g __fish_git_prompt_char_upstream_behind ' ↓ '
+set -g __fish_git_prompt_char_upstream_diverged ' ﱟ⇄ '
+set -g __fish_git_prompt_char_upstream_equal ' = '
+set -g __fish_git_prompt_char_upstream_prefix ''''
+
+bind \b backward-kill-word
+
+thefuck --alias | source
---
+++ functions/fish_greeting.fish
@@ -0,0 +1,9 @@
+function fish_greeting
+	# Customize fish greeting message
+	neofetch | lolcat
+	echo
+	echo
+	echo
+	echo
+	echo
+end
